91ced28cda6e10aff7cdacd94f93881f3ccc8e80
Ery Lee <ery.lee@gmail.com>
2015-03-01 13:18:39 +0800

    max_connections -> max_clients

--- examples/ssl/src/ssl_echo_server_app.erl
+++ examples/ssl/src/ssl_echo_server_app.erl
@@ -18,7 +18,7 @@ start(_StartType, _StartArgs) ->
     ok = esockd:start(),
     {ok, Sup} = ssl_echo_server_sup:start_link(),
     SslOpts = [{certfile, "./crt/demo.crt"}, {keyfile,  "./crt/demo.key"}], %{cacertfile, "./crt/cacert.pem"}, 
-    SockOpts = [binary, {reuseaddr, true}, {max_connections, 1000}, {ssl, SslOpts}],
+    SockOpts = [binary, {reuseaddr, true}, {acceptor_pool, 4}, {max_clients, 1000}, {ssl, SslOpts}],
     {ok, _} = esockd:open('echo/ssl', 5000, SockOpts, ssl_echo_server),
     {ok, Sup}.
 
