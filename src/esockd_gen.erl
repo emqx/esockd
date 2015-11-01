fbd76c7187a7ce52240152929cd2c9818c19cd35
Feng Lee <ery.lee@gmail.com>
2015-11-01 10:17:27 +0800

    gen

---
+++ src/esockd_gen.erl
@@ -0,0 +1,44 @@
+%%%-----------------------------------------------------------------------------
+%%% Copyright (c) 2014-2015 eMQTT.IO, All Rights Reserved.
+%%%
+%%% Permission is hereby granted, free of charge, to any person obtaining a copy
+%%% of this software and associated documentation files (the "Software"), to deal
+%%% in the Software without restriction, including without limitation the rights
+%%% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+%%% copies of the Software, and to permit persons to whom the Software is
+%%% furnished to do so, subject to the following conditions:
+%%%
+%%% The above copyright notice and this permission notice shall be included in all
+%%% copies or substantial portions of the Software.
+%%%
+%%% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+%%% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+%%% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+%%% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+%%% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+%%% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+%%% SOFTWARE.
+%%%-----------------------------------------------------------------------------
+%%% @doc
+%%% eSockd general functions.
+%%%
+%%% @end
+%%%-----------------------------------------------------------------------------
+-module(esockd_gen).
+
+-export([send_fun/1, async_send_fun/1]).
+
+-spec send_fun(esockd_connection:connection()) -> fun().
+send_fun(Connection) ->
+     fun(Data) -> Connection:send(Data) end.
+
+-spec async_send_fun(esockd_connection:connection()) -> fun().
+async_send_fun(Connection) ->
+    fun(Data) ->
+        try Connection:async_send(Data) of
+            true -> ok
+        catch
+            error:Error -> exit({shutdown, Error})
+        end
+    end.
+
