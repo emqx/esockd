4abcfc9d087df5f9797c48db6606fb435c5e589a
Feng <ery.lee@gmail.com>
2016-01-31 15:19:09 +0800

    -type option()

--- src/esockd.erl
+++ src/esockd.erl
@@ -61,15 +61,14 @@
 
 -type mfargs() :: atom() | {atom(), atom()} | {module(), atom(), [term()]}.
 
--type option() ::
-		{acceptors, pos_integer()} |
-		{max_clients, pos_integer()} |
-        {access, [esockd_access:rule()]} |
-        {shutdown, brutal_kill | infinity | pos_integer()} |
-        {tune_buffer, false | true} |
-        {logger, atom() | {atom(), atom()}} |
-        {ssl, list()} | %%TODO: [ssl:ssloption()]
-        {sockopts, [gen_tcp:listen_option()]}.
+-type option() :: {acceptors, pos_integer()}
+                | {max_clients, pos_integer()}
+                | {access, [esockd_access:rule()]}
+                | {shutdown, brutal_kill | infinity | pos_integer()}
+                | {tune_buffer, false | true}
+                | {logger, gen_logger:logcfg()}
+                | {ssl, list()} %%TODO: [ssl:ssloption()]
+                | {sockopts, [gen_tcp:listen_option()]}.
 
 -export_type([ssl_socket/0, sock_fun/0, sock_args/0, tune_fun/0, mfargs/0, option/0]).
 
