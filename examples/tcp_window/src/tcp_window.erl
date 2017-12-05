37280774cbc3818c6ea4838da61688b95939b2c1
Feng Lee <feng@emqtt.io>
2017-12-05 09:41:47 +0800

    Add an example for TCP window full issue

--- examples/tcp_window/src/tcp_window.erl
+++ examples/tcp_window/src/tcp_window.erl
@@ -24,8 +24,8 @@
 
 -export([main/0, main/1]).
 
-%%callback 
--export([start_server/1, server_init/1, server_loop/1]).
+%% Callback 
+-export([start_server/2, server_init/2, server_loop/2]).
 
 -define(TCP_OPTIONS,
         [binary,
@@ -35,49 +35,55 @@
          {sndbuf, 1024},
          {send_timeout, 3000}]).
 
-main() -> main(5000).
+main() -> main(5000, sync).
 
-main([Port]) when is_atom(Port) ->
-    main(list_to_integer(atom_to_list(Port)));
+main([Port, Type]) when is_atom(Port) ->
+    main(list_to_integer(atom_to_list(Port)), Type).
 
-main(Port) when is_integer(Port) ->
+main(Port, Type) when is_integer(Port) ->
     lists:foreach(fun application:ensure_all_started/1, [sasl, crypto, gen_logger, esockd]),
     SockOpts = [{access, [{allow, all}]},
                 {acceptors, 1}, 
                 {shutdown, infinity},
                 {max_clients, 100},
                 {sockopts, ?TCP_OPTIONS}],
-    esockd:open(tcp_block, Port, SockOpts, {?MODULE, start_server, []}),
+    esockd:open(tcp_block, Port, SockOpts, {?MODULE, start_server, [Type]}),
     start_client(Port).
 
-start_server(Conn) ->
-	{ok, spawn_link(?MODULE, server_init, [Conn])}.
+start_server(Conn, Type) ->
+	{ok, spawn_link(?MODULE, server_init, [Conn, Type])}.
 
-server_init(Conn) ->
-    {ok, NewConn} = Conn:wait(),
+server_init(Conn0, Type) ->
+    {ok, Conn} = Conn0:wait(),
     io:format("sockopts: ~p~n", [Conn:getopts([send_timeout, send_timeout_close])]),
-    server_loop(NewConn).
+    server_loop(Conn, send_fun(Type, Conn)).
 
-server_loop(Conn) ->
+server_loop(Conn, SendFun) ->
 	case Conn:recv(0) of
 		{ok, Data} ->
 			{ok, PeerName} = Conn:peername(),
             io:format("server recv: ~p from (~s)~n", [Data, esockd_net:format(peername, PeerName)]),
-			Conn:send(Data),
+			SendFun(Data),
             %% flood the tcp window of client
-            send_loop(Conn, Data, 0);
-			%% server_loop(Conn);
+            send_loop(Conn, SendFun, Data, 0);
 		{error, Reason} ->
             io:format("server tcp error ~s~n", [Reason]),
             Conn:fast_close(),
 			{stop, Reason}
 	end.
 
-send_loop(Conn, Data, Count) ->
-    case Conn:send(Data) of
+send_loop(Conn, SendFun, Data, Count) ->
+    case SendFun(Data) of
+        true ->
+            io:format("Send ~w~n", [Count]),
+            io:format("Stats: ~p~n", [Conn:getstat([send_cnt])]),
+            send_loop(Conn, SendFun, Data, Count + iolist_size(Data));
+        false ->
+            io:format("False Send ~w~n", [Count]),
+            send_loop(Conn, SendFun, Data, Count + iolist_size(Data));
         ok ->
             io:format("Send ~w~n", [Count]),
-            send_loop(Conn, Data, Count + iolist_size(Data));
+            send_loop(Conn, SendFun, Data, Count + iolist_size(Data));
         {error, Error} ->
             io:format("Send error: ~p~n", [Error]),
             Conn:fast_close()
@@ -98,9 +104,18 @@ client_loop(Sock, I) ->
     case gen_tcp:recv(Sock, 0) of
         {ok, Data} ->
             io:format("client recv: ~p~n", [Data]),
-            client_loop(Sock, I+1); 
+            client_loop(Sock, I+1);
         {error, Reason} ->
             io:format("client tcp error: ~p~n", [Reason]),
             gen_tcp:close(Sock)
     end.
+
+send_fun(sync, Conn) ->
+    fun(Data) -> Conn:send(Data) end;
+send_fun(async, Conn) ->
+    fun(Data) -> port_command(Conn:sock(), Data) end;
+send_fun(async_force, Conn) ->
+    fun(Data) -> port_command(Conn:sock(), Data, [force]) end;
+send_fun(async_nosuspend, Conn) ->
+    fun(Data) -> port_command(Conn:sock(), Data, [nosuspend]) end.
 
